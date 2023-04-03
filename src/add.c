499906c74c129bdce1ffa156ccce5ebfc14f1c07
ajay kumar <ajaykumarbingi0714@gmail.com>
2023-04-03 12:28:59 +0530

    fix #3

---
+++ src/add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<strings.h>
+int main()
+{
+    int a,b;
+    printf("enter two numbers\n");
+    scanf("%d %d",&a,&b);
+    printf("addition of two number's is %d\n",a+b);
+     printf("multiplicatio  of two number's is %d\n",a*b);
+    return 0;   
+}
